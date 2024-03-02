b71639c773572df30e6296a776ed940140ef8a1e
Deivison Isidoro <deivisonisidoro@gmail.com>
2024-03-02 19:08:04 -0300

    chore: adding swagger and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes( new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle("Title Documentation")
+  .setDescription("Description Documentation")
+  .setVersion("1.0.0")
+  .addTag("Users")
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("", app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
